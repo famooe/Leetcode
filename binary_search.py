7a1556cd4dc45e1deb78f75a3d01347e79556fb3
famooe <60573780+famooe@users.noreply.github.com>
2021-05-21 07:51:26 -0400

    Create binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def search(self, nums, target):       
+        first = 0
+        last = len(nums) - 1
+        while first <= last:
+            mid_point = first + (last - first + 1) // 2
+            mid_value = nums[mid_point]
+            if mid_value == target:
+                return mid_point
+            elif target < mid_value:
+                last = mid_point - 1
+            else:
+                first = mid_point + 1
+        return -1
