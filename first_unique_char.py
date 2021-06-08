2a9fbd5909b74870d30ecdb04792859c854b1634
famooe <60573780+famooe@users.noreply.github.com>
2021-06-07 23:39:37 -0400

    Update first_unique_char.py

--- first_unique_char.py
+++ first_unique_char.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def firstUniqChar(self, s):
         count = collections.Counter(s)
-        
+        # print(count.items())            # is a list=[(char1, iter1), (char2, iter2), ...]
         for index , char in enumerate(s):
             if count[char] == 1:
                 return index
