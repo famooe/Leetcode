bc7c10a018bcd7ce8766439f560a2af98ededc72
famooe <60573780+famooe@users.noreply.github.com>
2021-06-09 19:27:29 -0400

    Create excel_sheet_column_number.py

---
+++ excel_sheet_column_number.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def titleToNumber(self, columnTitle):
+        total, count = 0, 0
+        for i in columnTitle[::-1]:
+            total += (ord(i) - 64) * (26 ** count)             #ord('A') = 65
+            count += 1
+            print(total)
+        return total
